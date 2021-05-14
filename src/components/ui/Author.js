059345cbf0f4a689864a936d986c2eafc3880d8e
deepvashisth <deepvashisth7777@gmail.com>
2021-05-14 13:43:38 +0530

    first commit

---
+++ src/components/ui/Author.js
@@ -0,0 +1,15 @@
+import react from 'react'
+
+const Author = () =>{
+  return (
+    <div className="center">
+    <br />
+    Made with ❤ by Deep vashisth
+    <br />
+    <br />
+    <br />
+    </div>
+  )
+}
+
+export default Author
